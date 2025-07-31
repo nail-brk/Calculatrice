3ed03e22ee3411e7f9817d08ceefa994e3969e92
nail-brk <briknail@gmail.com>
2025-07-31 20:03:12 +0200

    Fonction principale

--- calculette.cpp
+++ calculette.cpp
@@ -1,8 +1,13 @@
-void afficherResultat(double resultat, bool erreur, char op) {
-    if (erreur) {
-        if (op == '/') cout << "Erreur : division par zéro !" << endl;
-        else cout << "Erreur : opérateur invalide !" << endl;
-    } else {
-        cout << "Résultat : " << resultat << endl;
-    }
+int main() {
+    double num1, num2;
+    char op;
+    bool erreur;
+
+    cout << "Entrez une opération (ex: 5 + 2) : ";
+    cin >> num1 >> op >> num2;
+
+    double resultat = calculer(num1, num2, op, erreur);
+    afficherResultat(resultat, erreur, op);
+
+    return erreur ? 1 : 0;
 }
