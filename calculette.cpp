73dcaab524f62fc3a1e7bc1b567f5f85353791d2
nail-brk <briknail@gmail.com>
2025-07-31 20:00:17 +0200

    Les calculs possibles

--- calculette.cpp
+++ calculette.cpp
@@ -1,7 +1,3 @@
-#include <iostream>
-using namespace std;
-
-
 double calculer(double a, double b, char op, bool& erreur) {
     erreur = false;
 
@@ -19,28 +15,4 @@ double calculer(double a, double b, char op, bool& erreur) {
             erreur = true;
             return 0;
     }
-}
-
-
-void afficherResultat(double resultat, bool erreur, char op) {
-    if (erreur) {
-        if (op == '/' || op == '*') cout << "Erreur : division par zéro !" << endl;
-        else cout << "Erreur : opérateur invalide !" << endl;
-    } else {
-        cout << "Résultat : " << resultat << endl;
-    }
-}
-
-int main() {
-    double num1, num2;
-    char op;
-    bool erreur;
-
-    cout << "Entrez une opération (ex: 5 + 2) : ";
-    cin >> num1 >> op >> num2;
-
-    double resultat = calculer(num1, num2, op, erreur);
-    afficherResultat(resultat, erreur, op);
-
-    return erreur ? 1 : 0;
 }
