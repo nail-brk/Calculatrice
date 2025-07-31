ac9d4bbda25e5565765a12ede2dc1e320a555ae8
nail-brk <briknail@gmail.com>
2025-07-31 20:01:41 +0200

    Affichage des résultats

--- calculette.cpp
+++ calculette.cpp
@@ -1,18 +1,8 @@
-double calculer(double a, double b, char op, bool& erreur) {
-    erreur = false;
-
-    switch (op) {
-        case '+': return a + b;
-        case '-': return a - b;
-        case '*': return a * b;
-        case '/':
-            if (b == 0) {
-                erreur = true;
-                return 0;
-            }
-            return a / b;
-        default:
-            erreur = true;
-            return 0;
+void afficherResultat(double resultat, bool erreur, char op) {
+    if (erreur) {
+        if (op == '/') cout << "Erreur : division par zéro !" << endl;
+        else cout << "Erreur : opérateur invalide !" << endl;
+    } else {
+        cout << "Résultat : " << resultat << endl;
     }
 }
